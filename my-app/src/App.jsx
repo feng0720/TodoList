d2722d8aba45e2937e214cf96112ea7283672748
feng0720 <fengnuan720@gmail.com>
2025-09-22 21:10:59 +0800

    check the checkbox

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,27 +4,33 @@ import Button from './components/button';
 import List from './components/List';
 import Addthing from './components/Addthing';
 
-const list = [
-  {
-    context:"breath",
-    isFinished:true,
-    date:"2025-9-18"
-  },
-  {
-    context:"eat breakfast",
-    isFinished:false,
-    date:"2025-9-18"
-  }
-];
+const now = new Date();
+const year = now.getFullYear();
+const month = String(now.getMonth()+1).padStart(2,'0'); // 保持格式保证都是两位 padStart(targetLength,"string"果一个字符串的长度小于targetLength会在他的前面添加string直到等于targetLength
+// const week = String(now.getDay()).padStart(2,'0'); // 这里的getDay是获得一个星期的第几天即星期几
+const day = String(now.getDate()).padStart(2,'0'); // 获取今天是几号
+const today = `${year}-${month}-${day}`;
 
 
 function App(){
+  const [list,setList] = useState([
+  {
+    context:"呼吸",
+    isFinished:true,
+    date: today
+  },
+  {
+    context:"吃早饭",
+    isFinished:false,
+    date: today
+  }]
+)
+
   const [showAdd,setShowAdd] = useState(false);
   // 实现添加的输入框的显示
   function handleShowAdd(){
     setShowAdd(true);
   }
-  const [todo,setTodo] = useState(list);
   const [formData,setFormData] = useState({
     context:"",
     isFinished: false,
@@ -51,26 +57,22 @@ function App(){
       return;
     }
     // 如果没输入时间就默认今天
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = String(now.getMonth()+1).padStart(2,'0'); // 保持格式保证都是两位 padStart(targetLength,"string")如果一个字符串的长度小于targetLength会在他的前面添加string直到等于targetLength
-    // const week = String(now.getDay()).padStart(2,'0'); // 这里的getDay是获得一个星期的第几天即星期几
-    const day = String(now.getDate()).padStart(2,'0'); // 获取今天是几号
-    const thedate = `${year}-${month}-${day}`;
-    // 这里需要新建一个项目存储因为setTodo是异步的用这个去更新保证formData和todo是同步更新的    
+    // 这里需要新建一个项目存储因为setList是异步的用这个去更新保证formData和list是同步更新的    
     const itemdate = {
       ...formData,
-      date:formData.date.trim()?formData.date:thedate
+      date:formData.date.trim()?formData.date:today
     }
-    setTodo([
-      ...todo,
+    setList([
+      ...list,
       itemdate
     ]);
     setFormData({context:"",isFinished: false,date:""});
   }
+  // 实现删除项目
   function handleRemove(key){
-    setTodo(todo.filter(el=>el.context!==key));
+    setList(list.filter(el=>el.context!==key));
   }
+  // 实现简单的用户登录
   const [name,setName] = useState("");
   const [show,setShow] = useState(false);
   const handleName = (e)=>{
@@ -78,21 +80,32 @@ function App(){
     setName(e.target.value);
   }
   const handleShow = ()=>{
-    name?setShow(true):null;
+    name?setShow(true):null; // 只有输入用户名才显示项目
   }
+
+  const handleToggle = (context)=>{
+    setList(prev=>
+      prev.map(item=>item.context===context?{...item,isFinished:!item.isFinished}:item)
+    )
+  }
+
   return (
     <>
       {!show&&<div>
-        <input type="text" className='border-white border-2' placeholder='请输入用户名字' onChange={handleName}/>
+        <input type="text" className='border-white border-2' placeholder='请输入用户名字' onChange={handleName} onKeyDown={(event)=>{ // 按下enter也可以confirm和按钮功能一样
+          if(event.key==="Enter"){
+            handleShow();
+          }
+        }}/>
         <button className='block m-auto mt-4 border-solid bg-white text-black rounded-lg p-2' onClick={handleShow}>confirm</button>
       </div>}
       {show&&<div>
-        <h1 className='text-3xl font-1000 text-blue-300 '>{name.toUpperCase()}`ToDoList</h1>
+        <h1 className='text-3xl font-1000 text-blue-300'>{name.toUpperCase()}`listList</h1>
         <Button onClick={handleShowAdd} children={"Add"} classname={"bg-blue-500 mt-4 mb-4"}></Button>
         {showAdd&&<Addthing handleChange={handleChange} context={handleContext} date={handleDate} value={formData} classname="bg-blue-500"/>}
-        <h2>The number of your things is {todo.length}</h2>
+        <h2 className='mb-4 text-green-200 text-lg'>The number of your things is {list.length}</h2>
         <hr />
-        <List list={todo} remove={handleRemove}/>
+        <List list={list} remove={handleRemove} toggle={handleToggle}/>
       </div>}
     </>
   )
--- my-app/src/components/List.jsx
+++ my-app/src/components/List.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Button from "./button";
 
-export default function List({list ,remove}){
+export default function List({list ,remove ,toggle}){
+  // 创建一个状态对象来存储每个项目是否checked--这个是在List里面使用useState的方法，目前是使用在App里面使用的方法
+  // const [check,setCheck] = useState({});
+
   return (
     <>
       <ul>
@@ -9,15 +13,32 @@ export default function List({list ,remove}){
             <li key={item.context} className="flex items-center justify-between mt-2 mb-2">
               <div className="flex items-center space-x-2">
                 <label htmlFor="item">
-                  <input type="checkbox" />
+                  {/* 设置初始的checked为false 如果改变了的话就根据context来改变对应的checked */}
+                  {/* 最先开始这里的item.context是undefined属于非受控组件如果突然变成受控组件会报错这里使用!!保证其一直受控 */}
+                  {/* <input type="checkbox" checked={!!check[item.context]} onChange={(e)=>{
+                    setCheck({
+                      ...check,
+                      [item.context]:e.target.checked
+                    })
+                  }}/> */}
+                  <input type="checkbox" name="finish" id="finish" checked={item.isFinished} onChange={()=>toggle(item.context)} />
                 </label>
-                <span>{" "+item.context+"  --date: "+item.date}</span>
+                {/* <span className={check[item.context]?"text-gray-600":""}>{" "+item.context+"  --date: "+item.date}</span> */}
+                <span className={item.isFinished?"text-gray-600":""}>{" "+item.context+"  --date: "+item.date}</span>
               </div>
               {/* 编辑事件 */}
               <div className="flex space-x-2">
-                <Button classname={"bg-green-500 mr-0"}>Edit</Button>
+                <Button classname={"bg-green-500 mr-1"} >Edit</Button>
                 {/* 删除事件如果还没完成需要询问是否需要删除 */}
-                <Button classname="bg-red-500 mr-0" onClick={()=>remove(item.context)}>Delete</Button>
+                <Button classname="bg-red-500 mr-0" onClick={()=>{
+                  if(!item.isFinished){
+                    if(confirm("这个任务还没有完成哦\n确定要半途而废吗?\n还是说这个任务被突然取消掉了呢?")){
+                      remove(item.context);
+                    }
+                  }else{
+                    remove(item.context);
+                  }
+                }}>Delete</Button>
               </div>
             </li>
           )
